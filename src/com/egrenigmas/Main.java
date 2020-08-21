89883166d368f4347283a87314a1b89662685df5
Tejas-Iyer <tejas.iyer@outlook.com>
2020-08-20 22:31:20 -0400

    Test 1 v1

--- src/com/egrenigmas/Main.java
+++ src/com/egrenigmas/Main.java
@@ -1,10 +1,16 @@
 package com.egrenigmas;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
+        Scanner in = new Scanner(System.in);
 
+        int n = in.nextInt();
+
+        System.out.println(n);
     }
 }
